a7547b412779828f694e16cce231a947900333e5
c1ose-d <michael.glodin2004@gmail.com>
2023-03-15 09:02:27 +0300

    Add project files.

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace UI {
-    public partial class MainWindow : Window {
-        private readonly struct Strings {
+﻿namespace UI
+{
+    public partial class MainWindow : Window
+    {
+        private readonly struct Strings
+        {
             public static string ChromeMaximize { get; } = "";
             public static string ChromeRestore { get; } = "";
         }
 
-        private void WindowStateChange() {
-            switch (WindowState) {
+        private void WindowStateChange()
+        {
+            switch (WindowState)
+            {
                 case WindowState.Normal:
                     WindowState = WindowState.Maximized;
                     break;
@@ -16,24 +21,54 @@
             }
         }
 
+        DateTime Start { get; set; }
+        private async Task TimerAsync()
+        {
+            try
+            {
+                TitleBarToolTip.StaysOpen = false;
+            }
+            catch { }
+            if (DateTime.Now >= Start.AddMinutes(5))
+            {
+                TitleBarToolTip.StaysOpen = true;
+                Message.Content = "Приложение закроется через 3 минуты!";
+            }
+            if (DateTime.Now >= Start.AddMinutes(10))
+            {
+                Application.Current.Shutdown();
+            }
+            await Task.Delay(10000);
+        }
+
         private SignInPage? SignInPage { get; set; }
         private MainPageFrame? MainPageFrame { get; set; }
-        public void InitializeAdditionalComponent() {
+        public void InitializeAdditionalComponent()
+        {
             SignInPage = new();
             MainPageFrame = MainPageFrame.GetInstance();
         }
 
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
             InitializeAdditionalComponent();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Start = DateTime.Now;
             _ = Frame.Navigate(SignInPage);
+            while (true)
+            {
+                await TimerAsync();
+            }
         }
 
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e) {
-            switch (WindowState) {
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            switch (WindowState)
+            {
                 case WindowState.Normal:
                     MaximizeButton.Content = Strings.ChromeMaximize;
                     break;
@@ -43,38 +78,48 @@
             }
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e) {
-            if (MainPageFrame != null && MainPageFrame.CanGoBack) {
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainPageFrame != null && MainPageFrame.CanGoBack)
+            {
                 MainPageFrame.GoBack();
             }
-            else {
+            else
+            {
                 Frame.GoBack();
             }
         }
 
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e) {
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
             WindowState = WindowState.Minimized;
         }
 
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e) {
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
             WindowStateChange();
         }
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e) {
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
             Application.Current.Shutdown();
         }
 
-        private void DockPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-            if (e.ClickCount == 2) {
+        private void DockPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
                 WindowStateChange();
             }
-            else {
+            else
+            {
                 WindowState = WindowState.Normal;
                 DragMove();
             }
         }
 
-        private void Frame_Navigated(object sender, NavigationEventArgs e) {
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
             BackButton.IsEnabled = Frame.CanGoBack;
         }
     }
